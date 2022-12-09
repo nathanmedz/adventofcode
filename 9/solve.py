8547ad92fb3d28c1b6c296de8bcae3c66bac3f6c
Nathan <nathan@tradablebits.com>
2022-12-09 15:20:24 -0800

    cleanup

--- 9/solve.py
+++ 9/solve.py
@@ -16,37 +16,39 @@ def move_tail(current_head, current_tail):
     return current_tail
 
 
+def move_head(current_head, direction):
+    return current_head[0] + direction[0], current_head[1] + direction[1]
+
+
 def part1():
-    visited = set()
-    visited.add((0,0))
-    current_head = (0, 0)
-    current_tail = (0, 0)
+    visited = {(0, 0)}
+    current_head, current_tail = (0, 0), (0, 0)
     with open('input.txt') as f:
         for line in f.readlines():
-            l = line.strip().split(' ')
-            direction, num = directions[l[0]], l[1]
+            direction_str, num = line.strip().split(' ')
             for i in range(int(num)):
-                current_head = (current_head[0] + direction[0], current_head[1] + direction[1])
+                current_head = move_head(current_head, directions[direction_str])
                 current_tail = move_tail(current_head, current_tail)
                 visited.add(current_tail)
+
     print(len(visited))
 
 
 def part2():
-    visited = set()
-    visited.add((0,0))
-    num_knots = 10
-    knot_list = [(0,0)] * num_knots
+    visited = {(0, 0)}
+    knot_list = [(0, 0)] * 10
     with open('input.txt') as f:
         for line in f.readlines():
-            l = line.strip().split(' ')
-            direction, num = directions[l[0]], l[1]
+            direction_str, num = line.strip().split(' ')
             for _ in range(int(num)):
-                knot_list[0] = (knot_list[0][0] + direction[0], knot_list[0][1] + direction[1])
-                for i, knot in enumerate(knot_list[:-1]):
+                knot_list[0] = move_head(knot_list[0], directions[direction_str])
+                for i in range(len(knot_list) - 1):
                     knot_list[i+1] = move_tail(knot_list[i], knot_list[i+1])
+
                 visited.add(knot_list[-1])
+
     print(len(visited))
 
+
 if __name__ == '__main__':
-    part1()
+    part2()
