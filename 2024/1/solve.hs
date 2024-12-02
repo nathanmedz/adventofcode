8338efd45067d320c53bb2a0a06c0c12f107ec1e
Nathan <nathan.medz@gmail.com>
2024-12-01 19:40:36 -0500

    Day 1

--- 2024/1/solve.hs
+++ 2024/1/solve.hs
@@ -1,7 +1,5 @@
-import Data.List
 
-strToInt :: String -> Int
-strToInt = read
+import Data.List
 
 first [] = []
 first (x:xs) = x:second xs
@@ -9,15 +7,19 @@ first (x:xs) = x:second xs
 second [] = []
 second (x:xs) = first xs
 
-partone = do
+readInt :: String -> Int
+readInt = read
+
+partone (left, right) = do
+  putStrLn $ "Part 1: " ++ show (sum (map abs (zipWith (-) (sort left) (sort right))))
+
+parttwo (left, right) = do
+  putStrLn $ "Part 2: " ++ show  (sum (map (\x -> x * length (filter (==x) right)) left))
+
+
+main = do 
   contents <- readFile "input.txt"
-  let intList = map strToInt $ words contents
-  let left = sort $ first intList
-  let right = sort $ second intList  
-  let zipped = zipWith (-) left right
-  let distances = map abs zipped
-  print $ sum distances
-
-
-main = 
-  do partone
+  let intList = map readInt $ words contents
+  let (left, right) = (first intList, second intList) 
+  partone(left, right)
+  parttwo(left, right)
