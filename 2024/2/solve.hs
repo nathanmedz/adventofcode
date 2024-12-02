ae0f212eb390d36a1c310c2599f03f23167c7a21
Nathan <nathan.medz@gmail.com>
2024-12-02 07:39:48 -0500

    stub for day 2

---
+++ 2024/2/solve.hs
@@ -0,0 +1,15 @@
+import Data.List (sort)
+
+
+partOne = do
+  print "nothing"
+
+
+partTwo = do
+  print "nothing"
+
+
+main :: IO ()
+main = do
+  contents <- readFile "test.txt"
+  print contents 
