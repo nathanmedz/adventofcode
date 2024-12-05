a96059c44f561d0a863c093d1167406c1bd7309e
Nathan Meadows <nathan@scribenote.com>
2024-12-05 12:14:28 -0500

    day 5

--- 2024/5/solve.py
+++ 2024/5/solve.py
@@ -1,4 +1,38 @@
 import argparse
+from collections import defaultdict
+
+
+def seperate_valid(rule_dict, lines):
+    valid = []
+    invalid = []
+    for line in lines:
+        for i in line:
+            afters = rule_dict.get(i)
+            if not afters:
+                continue
+            if any(a in line and line.index(a) < line.index(i) for a in afters):
+                invalid.append(line)
+                break
+        else:
+            valid.append(line)
+    return valid, invalid
+
+
+def reorder_line(rule_dict, line):
+    for i in line:
+        afters = rule_dict.get(i)
+        if not afters:
+            continue
+        for a in afters:
+            if a not in line:
+                continue
+            a_idx = line.index(a)
+            i_idx = line.index(i)
+            if a_idx < i_idx:
+                line[a_idx], line[i_idx] = line[i_idx], line[a_idx]
+                return reorder_line(rule_dict, line)
+    else:
+        return line
 
 
 class Solver:
@@ -8,16 +42,27 @@ class Solver:
     def parse(self):
         file = "test.txt" if self.test else "input.txt"
         with open(file, "r") as f:
-            for line in f.read().strip().split("\n"):
-                yield line
+            rules, lines = f.read().strip().split("\n\n")
+            rules = [list(map(int, i.split("|"))) for i in rules.split("\n")]
+            lines = [list(map(int, i.split(","))) for i in lines.split("\n")]
+            return rules, lines
 
     def part1(self):
-        for line in self.parse():
-            pass
+        rules, lines = self.parse()
+        rule_dict = defaultdict(list)
+        for rule in rules:
+            rule_dict[rule[0]].append(rule[1])
+        valid, _ = seperate_valid(rule_dict, lines)
+        return sum([v[len(v) // 2] for v in valid])
 
     def part2(self):
-        for line in self.parse():
-            pass
+        rules, lines = self.parse()
+        rule_dict = defaultdict(list)
+        for rule in rules:
+            rule_dict[rule[0]].append(rule[1])
+        _, invalid = seperate_valid(rule_dict, lines)
+
+        return sum([reorder_line(rule_dict, v)[len(v) // 2] for v in invalid])
 
 
 if __name__ == "__main__":
