ea5c409f5142022df62cbade45705cc5515a05a2
Nathan Meadows <nathan@scribenote.com>
2024-12-06 11:50:25 -0500

    day 6

--- 2024/6/solve.py
+++ 2024/6/solve.py
@@ -1,6 +1,33 @@
 import argparse
 
 
+def input_as_dict(lines):
+    out = {}
+    for y, line in enumerate(lines):
+        for x, j in enumerate(list(line)):
+            out[(x, y)] = j
+    return out
+
+
+def turn_right(direction):
+    return (-direction[1], direction[0])
+
+
+def step(grid, x, y, direction, visited):
+    new_x, new_y = x + direction[0], y + direction[1]
+    next_step = grid.get((new_x, new_y))
+    if next_step is None:
+        raise ValueError()
+    if next_step == "#":
+        direction = turn_right(direction)
+        return x, y, direction
+    else:
+        if (new_x, new_y, direction) in visited:
+            raise StopIteration()
+        visited.add((new_x, new_y, direction))
+        return new_x, new_y, direction
+
+
 class Solver:
     def __init__(self, test=False):
         self.test = test
@@ -8,16 +35,46 @@ class Solver:
     def parse(self):
         file = "test.txt" if self.test else "input.txt"
         with open(file, "r") as f:
-            for line in f.read().strip().split("\n"):
-                yield line
+            return input_as_dict(f.read().strip().split("\n"))
 
     def part1(self):
-        for line in self.parse():
-            pass
+        grid = self.parse()
+        for coords, char in grid.items():
+            if char == "^":
+                direction = (0, -1)
+                x, y = coords
+                break
+
+        visited = {(x, y, direction)}
+        try:
+            while True:
+                x, y, direction = step(grid, x, y, direction, visited)
+        except ValueError:
+            return len(set([(i[0], i[1]) for i in visited]))
 
     def part2(self):
-        for line in self.parse():
-            pass
+        grid = self.parse()
+        for coords, char in grid.items():
+            if char == "^":
+                start_direction = (0, -1)
+                start_x, start_y = coords
+                break
+
+        loops = 0
+        for coords, char in grid.items():
+            if char == ".":
+                x, y, direction = start_x, start_y, start_direction
+                visited = {(x, y, direction)}
+                new_grid = grid.copy()
+                new_grid[coords] = "#"
+                try:
+                    while True:
+                        x, y, direction = step(new_grid, x, y, direction, visited)
+                except ValueError:
+                    continue
+                except StopIteration:
+                    loops += 1
+        return loops
 
 
 if __name__ == "__main__":
