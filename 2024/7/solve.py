15c4e3a52eb51518b43eaf4e7e8c4b295223eb00
Nathan <nathan.medz@gmail.com>
2024-12-07 07:39:39 -0500

    day 7

--- 2024/7/solve.py
+++ 2024/7/solve.py
@@ -1,4 +1,5 @@
 import argparse
+from operator import mul, add
 
 
 class Solver:
@@ -9,15 +10,47 @@ class Solver:
         file = "test.txt" if self.test else "input.txt"
         with open(file, "r") as f:
             for line in f.read().strip().split("\n"):
-                yield line
+                total, vals = line.split(":")
+                vals = [int(i) for i in vals.split()]
+                total = int(total)
+                yield total, vals
 
     def part1(self):
-        for line in self.parse():
-            pass
+        return sum(
+            [
+                total
+                for total, vals in self.parse()
+                if bfs(total, vals.pop(0), vals.copy(), [mul, add])
+            ]
+        )
 
     def part2(self):
-        for line in self.parse():
-            pass
+        return sum(
+            [
+                total
+                for total, vals in self.parse()
+                if bfs(total, vals.pop(0), vals.copy(), [mul, add, cat])
+            ]
+        )
+
+
+def bfs(total, current, vals, ops):
+    queue = [[current, vals]]
+    while queue:
+        current, vals = queue.pop(0)
+        if current == total:
+            return True
+        if current > total or not vals:
+            continue
+        next_vals = vals.copy()
+        next_val = next_vals.pop(0)
+        for op in ops:
+            new_current = op(current, next_val)
+            queue.append([new_current, next_vals])
+
+
+def cat(a, b):
+    return int(f"{a}{b}")
 
 
 if __name__ == "__main__":
