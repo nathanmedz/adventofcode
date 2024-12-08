db9c15e1adab458b89bdb2895798448de52f0b3a
Nathan <nathan.medz@gmail.com>
2024-12-08 13:56:08 -0500

    update stub

--- 2024/23/solve.py
+++ 2024/23/solve.py
@@ -1,22 +1,14 @@
 import argparse
+from adventofcode.helpers.solver_base import SolverBase
 
 
-class Solver:
-    def __init__(self, test=False):
-        self.test = test
-
-    def parse(self):
-        file = "test.txt" if self.test else "input.txt"
-        with open(file, "r") as f:
-            for line in f.read().strip().split("\n"):
-                yield line
-
+class Solver(SolverBase):
     def part1(self):
-        for line in self.parse():
+        for line in self.parse_as_lines():
             pass
 
     def part2(self):
-        for line in self.parse():
+        for line in self.parse_as_lines():
             pass
 
 
@@ -26,7 +18,4 @@ if __name__ == "__main__":
     parser.add_argument("--test", action="store_true")
     args = parser.parse_args()
     solver = Solver(args.test)
-    if args.part == 2:
-        print(solver.part2())
-    else:
-        print(solver.part1())
+    print(solver.part2() if args.part == 2 else solver.part1())
