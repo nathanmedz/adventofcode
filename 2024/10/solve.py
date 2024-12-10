8520588ed994e384f6e8a683c14192890f247dd0
Nathan Meadows <nathan@scribenote.com>
2024-12-10 11:17:30 -0500

    day 10

--- 2024/10/solve.py
+++ 2024/10/solve.py
@@ -4,12 +4,56 @@ from adventofcode.helpers.solver_base import SolverBase
 
 class Solver(SolverBase):
     def part1(self):
-        for line in self.parse_as_lines():
-            pass
+        grid = self.parse_as_dict()
+        ans = 0
+        for loc, char in grid.items():
+            if char == "0":
+                distinct, _ = bfs(grid, loc)
+                ans += distinct
+        return ans
 
     def part2(self):
-        for line in self.parse_as_lines():
-            pass
+        grid = self.parse_as_dict()
+        ans = 0
+        for loc, char in grid.items():
+            if char == "0":
+                _, total = bfs(grid, loc)
+                ans += total
+        return ans
+
+
+directions = [
+    (0, 1),
+    (0, -1),
+    (1, 0),
+    (-1, 0),
+]
+
+
+def bfs(grid, start):
+    queue = [[start]]
+    distinct = set()
+    total = 0
+    while queue:
+        path = queue.pop(0)
+        current = path[-1]
+        curr_int = int(grid[current])
+        if curr_int == 9:
+            total += 1
+            distinct.add(current)
+            continue
+        for d in directions:
+            next_pos = (current[0] + d[0], current[1] + d[1])
+            if next_pos not in grid or next_pos in path:
+                continue
+            next_int = int(grid[next_pos])
+
+            if next_int - curr_int == 1:
+                new_path = path.copy()
+                new_path.append(next_pos)
+                queue.append((new_path))
+
+    return len(distinct), total
 
 
 if __name__ == "__main__":
