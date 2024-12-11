727a8b52968d9bea11527d6e2b414c25a0c289ad
Nathan <nathan.medz@gmail.com>
2024-12-11 07:05:54 -0500

    day 11 part 1

--- 2024/11/solve.py
+++ 2024/11/solve.py
@@ -2,10 +2,33 @@ import argparse
 from adventofcode.helpers.solver_base import SolverBase
 
 
+def transform(rock):
+    if rock == 0:
+        return [1]
+    elif len(str(rock)) % 2 == 0:
+        str_rock = str(rock)
+        return [
+            int(str_rock[: len(str_rock) // 2]),
+            int(str_rock[len(str_rock) // 2 :]),
+        ]
+    else:
+        return [rock * 2024]
+
+
+def step(rocks):
+    stepped = []
+    for rock in rocks:
+        stepped.extend(transform(rock))
+
+    return stepped
+
+
 class Solver(SolverBase):
     def part1(self):
-        for line in self.parse_as_lines():
-            pass
+        rocks = [int(i) for i in self.parse_as_line().split()]
+        for _ in range(25):
+            rocks = step(rocks)
+        return len(rocks)
 
     def part2(self):
         for line in self.parse_as_lines():
