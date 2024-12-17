1309a11a1e16054e6d60546b89fd0054b4ed05e9
Nathan <nathan.medz@gmail.com>
2024-12-17 07:50:17 -0500

    parse day 17

--- 2024/17/solve.py
+++ 2024/17/solve.py
@@ -3,9 +3,20 @@ from helpers.solver_base import SolverBase
 
 
 class Solver(SolverBase):
+    def parse(self):
+        file = "test.txt" if self.test else "input.txt"
+        with open(file, "r") as f:
+            registers, program = f.read().strip().split("\n\n")
+            a, b, c = registers.split("\n")
+            a = int(a.split(": ")[-1])
+            b = int(b.split(": ")[-1])
+            c = int(c.split(": ")[-1])
+            program = [int(i) for i in program.split(": ")[-1].split(",")]
+            return a, b, c, program
+
     def part1(self):
-        for line in self.parse_as_lines():
-            pass
+        a, b, c, program = self.parse()
+        print(a, b, c, program)
 
     def part2(self):
         for line in self.parse_as_lines():
