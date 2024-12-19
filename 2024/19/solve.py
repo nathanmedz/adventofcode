10431984c2e2d658e4295766c4357b56bc424ef0
Nathan <nathan.medz@gmail.com>
2024-12-19 15:47:24 -0500

    day 19

--- 2024/19/solve.py
+++ 2024/19/solve.py
@@ -1,15 +1,35 @@
 import argparse
 from helpers.solver_base import SolverBase
+import functools
 
 
 class Solver(SolverBase):
+    def parse(self):
+        file = "test.txt" if self.test else "input.txt"
+        with open(file, "r") as f:
+            towels, designs = f.read().split("\n\n")
+            towels = tuple(towels.strip().split(", "))
+            designs = tuple(designs.strip().split("\n"))
+            return towels, designs
+
     def part1(self):
-        for line in self.parse_as_lines():
-            pass
+        towels, designs = self.parse()
+        return sum([1 for design in designs if count_design(design, towels)])
 
     def part2(self):
-        for line in self.parse_as_lines():
-            pass
+        towels, designs = self.parse()
+        return sum([count_design(design, towels) for design in designs])
+
+
+@functools.cache
+def count_design(design, towels):
+    if design == "":
+        return True
+    return sum(
+        count_design(design.replace(towel, "", 1), towels)
+        for towel in towels
+        if design.startswith(towel)
+    )
 
 
 if __name__ == "__main__":
